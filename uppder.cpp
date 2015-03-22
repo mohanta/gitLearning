c5e8d20260f958bfa37aac6a101aab5896772c68
mohanta <mohanta.chinmay@gmail.com>
2015-03-22 17:27:08 +0530

    initial comment

---
+++ uppder.cpp
@@ -0,0 +1,19 @@
+#define <iostream>
+#define <cstdlib>
+#define <fstream>
+
+using namespace std;
+
+int main(void){
+
+ifstream = in("test.txt");
+ofstream = out("utest.txt");
+
+int c;
+
+
+
+
+
+return 0;
+}
